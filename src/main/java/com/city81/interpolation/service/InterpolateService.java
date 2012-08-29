3265d192df4958241c1e01e9c0668270cab0b822
Geraint Jones <city81@btinternet.com>
2012-08-29 20:18:23 +0100

    Updated to handle odds steps

--- src/main/java/com/city81/interpolation/service/InterpolateService.java
+++ src/main/java/com/city81/interpolation/service/InterpolateService.java
@@ -13,10 +13,13 @@ import java.util.concurrent.RecursiveTask;
  */
 public class InterpolateService {
 
-	static final String VALUE_ARG_NOT_NULL_EXCP_TEXT = "value arguments cannot be null.";
-	static final String STEPS_NOT_VALID_EXCP_TEXT = "steps argument must not be null and must be divisable by two.";
-	static final String VALUE_LIST_NOT_VALID_EXCP_TEXT = "value list cannot be null or contain less than two values.";
-			
+	static final String VALUE_ARG_NOT_NULL_EXCP_TEXT 
+		= "value arguments cannot be null.";
+	static final String STEPS_ARG_NOT_VALID_EXCP_TEXT 
+		= "steps argument cannot be null or less than one.";
+	static final String VALUE_LIST_NOT_VALID_EXCP_TEXT 
+		= "value list cannot be null or contain less than two values.";
+
 	// parallel task executor
 	private final ForkJoinPool forkJoinPool = new ForkJoinPool();
 
@@ -32,8 +35,8 @@ public class InterpolateService {
 	 *            the number of steps to be interpolated
 	 * @return a linear interpolated list of numbers
 	 * @throws IllegalArgumentException
-	 *             if any of the arguments are null or if the steps arg is not
-	 *             divisable by two
+	 *             if any of the arguments are null, or 
+	 *             if the steps arg is less than one
 	 */
 	public List<Double> interpolate(Double valueOne, Double valueTwo,
 			Integer steps) throws IllegalArgumentException {
@@ -41,12 +44,12 @@ public class InterpolateService {
 		if ((valueOne == null) || (valueTwo == null)) {
 			throw new IllegalArgumentException(VALUE_ARG_NOT_NULL_EXCP_TEXT);
 		}
-		if ((steps == null) || (steps == 0) || (steps % 2 != 0)) {
-			throw new IllegalArgumentException(STEPS_NOT_VALID_EXCP_TEXT);
+		if ((steps == null) || (steps < 1)) {
+			throw new IllegalArgumentException(STEPS_ARG_NOT_VALID_EXCP_TEXT);
 		}
 
-		ForkJoinTask<List<Double>> job = forkJoinPool
-				.submit(new InterpolateTask(valueOne, valueTwo, steps));
+		ForkJoinTask<List<Double>> job = forkJoinPool.submit(
+				new InterpolateTask(valueOne, valueTwo, steps));
 		return job.join();
 	}
 
@@ -59,14 +62,18 @@ public class InterpolateService {
 	 * @param steps
 	 *            the number of steps to be interpolated
 	 * @return a linear interpolated list of numbers
+	 * @throws IllegalArgumentException
+	 *             if the value list is null or has less than two elements, or 
+	 *             if the steps arg is null or less than one
 	 */
-	public List<Double> interpolate(List<Double> valueList, Integer steps) throws IllegalArgumentException {
+	public List<Double> interpolate(List<Double> valueList, Integer steps)
+			throws IllegalArgumentException {
 
 		if ((valueList == null) || (valueList.size() < 2)) {
 			throw new IllegalArgumentException(VALUE_LIST_NOT_VALID_EXCP_TEXT);
 		}
-		if ((steps == null) || (steps == 0) || (steps % 2 != 0)) {
-			throw new IllegalArgumentException(STEPS_NOT_VALID_EXCP_TEXT);
+		if ((steps == null) || (steps < 1)) {
+			throw new IllegalArgumentException(STEPS_ARG_NOT_VALID_EXCP_TEXT);
 		}
 
 		List<Double> interpolatedList = new ArrayList<Double>();
@@ -90,12 +97,14 @@ public class InterpolateService {
 	 */
 	private static class InterpolateTask extends RecursiveTask<List<Double>> {
 
+		private static final long serialVersionUID = 1L;
+		
 		private final double valueOne;
 		private final double valueTwo;
 		private final int steps;
 		private static final int LOWEST_STEP = 2;
 
-		protected InterpolateTask(double valueOne, double valueTwo, int steps) {
+		private InterpolateTask(double valueOne, double valueTwo, int steps) {
 			this.valueOne = valueOne;
 			this.valueTwo = valueTwo;
 			this.steps = steps;
@@ -112,20 +121,30 @@ public class InterpolateService {
 		protected List<Double> compute() {
 
 			List<Double> interpolatedArray = new ArrayList<Double>();
-			double interpolatedValue = interpolate(valueOne, valueTwo);
 
-			if (this.steps == LOWEST_STEP) {
+			// check if reached lowest unit of work ie a number not divisable 
+			// by two or steps is equal to two
+			if ((this.steps % 2 != 0) || (this.steps == LOWEST_STEP)) {
+				
 				interpolatedArray.add(valueOne);
-				interpolatedArray.add(interpolatedValue);
+
+				double diff = valueTwo - valueOne;
+				double intervalValue = diff / steps;
+				
+				for (int i=1;i<steps;i++) {
+					interpolatedArray.add(valueOne + (i * intervalValue));
+				}
 				interpolatedArray.add(valueTwo);
+				
 			} else {
+				double interpolatedValue = interpolate(valueOne, valueTwo);
+
 				InterpolateTask interpolateTask1 = new InterpolateTask(
 						valueOne, interpolatedValue, (steps / 2));
 				interpolateTask1.fork();
 				InterpolateTask interpolateTask2 = new InterpolateTask(
 						interpolatedValue, valueTwo, (steps / 2));
-				List<Double> interpolatedArrayTask2 = interpolateTask2
-						.compute();
+				List<Double> interpolatedArrayTask2 = interpolateTask2.compute();
 				List<Double> interpolatedArrayTask1 = interpolateTask1.join();
 				interpolatedArray.addAll(interpolatedArrayTask1.subList(0,
 						interpolatedArrayTask1.size() - 1));
@@ -147,6 +166,6 @@ public class InterpolateService {
 			return ((valueTwo - valueOne) / 2.0) + valueOne;
 		}
 
-	}
-
+	}	
+	
 }
