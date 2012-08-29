37c10b8baf25d755cf39c0b974b84b4544eec892
Geraint Jones <city81@btinternet.com>
2012-08-29 20:18:10 +0100

    Updated with odd step number tests

--- src/test/java/com/city81/interpolation/service/InterpolateServiceTest.java
+++ src/test/java/com/city81/interpolation/service/InterpolateServiceTest.java
@@ -21,8 +21,7 @@ public class InterpolateServiceTest {
 	public void testInterpolateNullValueOne() {
 		
 		try {
-			List<Double> interpolatedList = 
-				interpolateService.interpolate(null, 25.0, 4);
+			interpolateService.interpolate(null, 25.0, 4);
 		} catch(Exception exc) {
 			assertTrue(exc instanceof IllegalArgumentException);
 			assertEquals(InterpolateService.VALUE_ARG_NOT_NULL_EXCP_TEXT, exc.getMessage());
@@ -49,7 +48,7 @@ public class InterpolateServiceTest {
 			interpolateService.interpolate(5.0, 25.0, null);
 		} catch(Exception exc) {
 			assertTrue(exc instanceof IllegalArgumentException);
-			assertEquals(InterpolateService.STEPS_NOT_VALID_EXCP_TEXT, exc.getMessage());
+			assertEquals(InterpolateService.STEPS_ARG_NOT_VALID_EXCP_TEXT, exc.getMessage());
 		}
 		
 	}	
@@ -61,22 +60,10 @@ public class InterpolateServiceTest {
 			interpolateService.interpolate(5.0, 25.0, 0);
 		} catch(Exception exc) {
 			assertTrue(exc instanceof IllegalArgumentException);
-			assertEquals(InterpolateService.STEPS_NOT_VALID_EXCP_TEXT, exc.getMessage());
+			assertEquals(InterpolateService.STEPS_ARG_NOT_VALID_EXCP_TEXT, exc.getMessage());
 		}
 		
 	}		
-	
-	@Test
-	public void testInterpolateNotDivisableByTwoSteps() {
-		
-		try {
-			interpolateService.interpolate(5.0, 25.0, 5);
-		} catch(Exception exc) {
-			assertTrue(exc instanceof IllegalArgumentException);
-			assertEquals(InterpolateService.STEPS_NOT_VALID_EXCP_TEXT, exc.getMessage());
-		}
-		
-	}	
 	
 	@Test
 	public void testInterpolateNullValueList() {
@@ -96,28 +83,43 @@ public class InterpolateServiceTest {
 		List<Double> interpolatedList = 
 				interpolateService.interpolate(5.0, 25.0, 4);
 		
+		assertEquals(5, interpolatedList.size());
 		assertEquals(5.0, interpolatedList.get(0), 0);
 		assertEquals(10.0, interpolatedList.get(1), 0);
 		assertEquals(15.0, interpolatedList.get(2), 0);
 		assertEquals(20.0, interpolatedList.get(3), 0);
 		assertEquals(25.0, interpolatedList.get(4), 0);
-	}
+	}	
+	
+	@Test
+	public void testInterpolateTwoValuesOddSteps() {
+		
+		List<Double> interpolatedList = 
+				interpolateService.interpolate(6.0, 18.0, 3);
+		
+		assertEquals(4, interpolatedList.size());		
+		assertEquals(6.0, interpolatedList.get(0), 0);
+		assertEquals(10.0, interpolatedList.get(1), 0);
+		assertEquals(14.0, interpolatedList.get(2), 0);
+		assertEquals(18.0, interpolatedList.get(3), 0);
+	}		
 
 	@Test
 	public void testInterpolateTwoValueList() {
 		
 		List<Double> inputList = new ArrayList<Double>();
 		inputList.add(5.0);
-		inputList.add(25.0);		
+		inputList.add(-25.0);		
 				
 		List<Double> interpolatedList = 
 				interpolateService.interpolate(inputList, 4);
 		
+		assertEquals(5, interpolatedList.size());
 		assertEquals(5.0, interpolatedList.get(0), 0);
-		assertEquals(10.0, interpolatedList.get(1), 0);
-		assertEquals(15.0, interpolatedList.get(2), 0);
-		assertEquals(20.0, interpolatedList.get(3), 0);
-		assertEquals(25.0, interpolatedList.get(4), 0);
+		assertEquals(-2.5, interpolatedList.get(1), 0);
+		assertEquals(-10.0, interpolatedList.get(2), 0);
+		assertEquals(-17.5, interpolatedList.get(3), 0);
+		assertEquals(-25.0, interpolatedList.get(4), 0);
 	}
 	
 	@Test
@@ -132,6 +134,7 @@ public class InterpolateServiceTest {
 		List<Double> interpolatedList = 
 				interpolateService.interpolate(inputList, 4);
 		
+		assertEquals(13, interpolatedList.size());		
 		assertEquals(5.0, interpolatedList.get(0), 0);
 		assertEquals(10.0, interpolatedList.get(1), 0);
 		assertEquals(15.0, interpolatedList.get(2), 0);
@@ -146,5 +149,36 @@ public class InterpolateServiceTest {
 		assertEquals(5.0, interpolatedList.get(11), 0);
 		assertEquals(0.0, interpolatedList.get(12), 0);
 	}
+		
+	@Test
+	public void testInterpolateFourValueListOddSteps() {
+		
+		List<Double> inputList = new ArrayList<Double>();
+		inputList.add(5.0);
+		inputList.add(25.0);
+		inputList.add(20.0);
+		inputList.add(0.0);		
+				
+		List<Double> interpolatedList = 
+				interpolateService.interpolate(inputList, 5);
+		
+		assertEquals(16, interpolatedList.size());		
+		assertEquals(5.0, interpolatedList.get(0), 0);
+		assertEquals(9.0, interpolatedList.get(1), 0);
+		assertEquals(13.0, interpolatedList.get(2), 0);
+		assertEquals(17.0, interpolatedList.get(3), 0);
+		assertEquals(21.0, interpolatedList.get(4), 0);
+		assertEquals(25.0, interpolatedList.get(5), 0);
+		assertEquals(24.0, interpolatedList.get(6), 0);
+		assertEquals(23.0, interpolatedList.get(7), 0);
+		assertEquals(22.0, interpolatedList.get(8), 0);
+		assertEquals(21.0, interpolatedList.get(9), 0);
+		assertEquals(20.0, interpolatedList.get(10), 0);
+		assertEquals(16.0, interpolatedList.get(11), 0);
+		assertEquals(12.0, interpolatedList.get(12), 0);
+		assertEquals(8.0, interpolatedList.get(13), 0);
+		assertEquals(4.0, interpolatedList.get(14), 0);
+		assertEquals(0.0, interpolatedList.get(15), 0);
+	}	
 	
 }
